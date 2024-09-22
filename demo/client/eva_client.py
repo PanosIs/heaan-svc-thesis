7c19aa76d042e718fff7e3de4c16a19094145dfc
Panagiotis Lantavos-Stratigakis <pstratig@flexport.com>
2024-09-22 21:05:22 +0200

    Move client and server to demo folder for final cleanup

---
+++ demo/client/eva_client.py
@@ -0,0 +1,166 @@
+import json
+import requests
+import click
+import random
+from sklearn.feature_extraction.text import CountVectorizer
+from eva import load, save
+from eva.seal import generate_keys
+import numpy
+from sklearn import datasets
+
+class Client:
+    def __init__(self) -> None:
+        # Initialize Vectorizer
+        print("Retrieving vocabulary from server and initializing vectorizer...")
+        self.vectorizer = CountVectorizer(ngram_range=(1,3))
+        vocab = requests.get(f"http://{ADDRESS}/vocabulary")
+        json_vocab = json.loads(vocab.text.replace("'", "\""))
+        self.vectorizer.vocabulary_ = json_vocab
+
+        # Create server session
+        print()
+        print("Creating server session...")
+        response = requests.request("POST", f"http://{ADDRESS}/session")
+        self.session = response.json()['session_uuid']
+        print(f"Session created with UUID {self.session}...")
+
+        # Get EVA signature and parameters
+        print("Retrieving encryption signature and parameters from server...")
+        r = requests.get(f"http://{ADDRESS}/session/{self.session}/signature")  
+        with open('client.svc.evasignature', 'wb') as f:
+            f.write(r.content)
+
+        r = requests.get(f"http://{ADDRESS}/session/{self.session}/params")  
+        with open('client.svc.evaparams', 'wb') as f:
+            f.write(r.content)
+
+        # Generate public-private key pair
+        print("Generating encryption context...")
+        params = load('client.svc.evaparams')
+        self.public_ctx, self.private_ctx = generate_keys(params)
+        save(self.public_ctx, "client.svc.sealpublic")
+
+        # Send public key to server
+        print("Uploading public key to server...")
+        url = f"http://{ADDRESS}/session/{self.session}/public"
+        files=[('file',('client.svc.sealpublic',open('client.svc.sealpublic','rb'),'application/octet-stream'))]
+        response = requests.request("POST", url, files=files)
+
+        # Get categories
+        self.categories = requests.get(f"http://{ADDRESS}/categories").json()["categories"]
+
+    # If index == -1 pick random index
+    def classify_indexed(self, index):
+        # Generate inputs
+        print("Encrypting input sample...")
+        test = datasets.fetch_20newsgroups(
+            subset='test',
+            remove=('headers', 'footers', 'quotes'),
+            categories=tuple(self.categories),
+            shuffle=False
+        )
+
+        if index == -1:
+            index = random.randint(0, len(test.data))
+
+        inputs = numpy.squeeze(numpy.asarray(self.vectorizer.transform(test.data).todense())).tolist()
+
+        print("Selected sample:")
+        print()
+        print(test.data[index])
+        print()
+    
+        # Encode an input
+        signature = load("client.svc.evasignature")
+        encrypted_input = self.public_ctx.encrypt({'x' : inputs[index]}, signature)
+        save(encrypted_input, "client.svc.encryptedin")
+
+        # Send input to server
+        print("Sending input to server for evaluation...")
+        url = f"http://{ADDRESS}/session/{self.session}/evaluate"
+        files=[('file',('client.svc.encryptedin',open('client.svc.encryptedin','rb'),'application/octet-stream'))]
+        response = requests.request("POST", url, files=files)
+        with open('client.svc.encryptedout', 'wb') as f:
+            f.write(response.content)
+
+        # Decrypt output
+        print("Decrypting server response...")
+        encrypted_output = load("client.svc.encryptedout")
+        output = self.private_ctx.decrypt(encrypted_output, signature)
+
+        # Evaluate output
+        print("Evaluating output classification...")
+        eval = []
+        for key in sorted(output.keys()):
+            eval.append(output[key][0])
+        i = numpy.argmax(eval)
+        print()
+        print("Classification:")
+        print(self.categories[i])
+        print()
+
+    def classify_custom(self, input):
+        inputs = numpy.squeeze(numpy.asarray(self.vectorizer.transform([input]).todense())).tolist()
+    
+        # Encode an input
+        signature = load("client.svc.evasignature")
+        encrypted_input = self.public_ctx.encrypt({'x' : inputs}, signature)
+        save(encrypted_input, "client.svc.encryptedin")
+
+        # Send input to server
+        print("Sending input to server for evaluation...")
+        url = f"http://{ADDRESS}/session/{self.session}/evaluate"
+        files=[('file',('client.svc.encryptedin',open('client.svc.encryptedin','rb'),'application/octet-stream'))]
+        response = requests.request("POST", url, files=files)
+        with open('client.svc.encryptedout', 'wb') as f:
+            f.write(response.content)
+
+        # Decrypt output
+        print("Decrypting server response...")
+        encrypted_output = load("client.svc.encryptedout")
+        output = self.private_ctx.decrypt(encrypted_output, signature)
+
+        # Evaluate output
+        print("Evaluating output classification...")
+        eval = []
+        for key in sorted(output.keys()):
+            eval.append(output[key][0])
+        i = numpy.argmax(eval)
+        print()
+        print("Classification:")
+        print(self.categories[i])
+        print()
+
+    def close_session(self):
+        # Close session
+        print("Closing server session...")
+        requests.post(f"http://{ADDRESS}/session/{self.session}/close")
+
+ADDRESS='172.18.0.2:80'
+
+@click.command()
+def classify():
+    client = Client()
+
+    mode = click.prompt(
+        'Choose client. Random will pick random samples to classify, indexed will allow you to specify the samples yourself while custom will allow you to enter custom input', 
+        type=click.Choice(['random', 'indexed', 'custom'], case_sensitive=False),
+        default='random',
+    )
+
+    iterations = click.prompt('Choose number of iterations (How many samples to classify), default 1', type=int, default=1)
+
+    for iteration in range(iterations):
+        if mode == 'random':
+            client.classify_indexed(-1)
+        elif mode == 'indexed':
+            index = click.prompt('Enter the index of the sample to classify', type=int)
+            client.classify_indexed(index)
+        elif mode == 'custom':
+            input = click.prompt('Enter the input to classify', type=str)
+            client.classify_custom(input)
+
+    client.close_session()
+
+if __name__ == '__main__':
+    classify()
---
+++ demo/server/eva_server.py
@@ -0,0 +1,238 @@
+import time
+import uuid
+import os
+import io
+from typing import List
+from eva import EvaProgram, Input, Output
+from eva import evaluate, save, load
+from eva.ckks import CKKSCompiler
+from flask import Flask, send_file
+from flask import request
+from sklearn import datasets, metrics
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+import json
+import requests
+from sklearn.feature_extraction.text import CountVectorizer
+from eva import load, save
+from eva.seal import generate_keys
+import numpy
+from sklearn import datasets
+
+CATEGORIES = ('comp.graphics', 'rec.autos', 'rec.sport.baseball', 'sci.crypt', 'talk.politics.mideast', 'soc.religion.christian',  'talk.politics.guns', 'rec.motorcycles',)
+NUM_FEATURES = 4096
+
+# Eva Application Wrapper
+
+class EvaApp:
+    def __init__(self) -> None:
+        self.sessions = {}
+        self.vectorizer = None
+        self.classifier = None
+        self.train_model()
+        self.create_eva_program()
+
+    def new_session(self) -> str:
+        session = EvaSession(self)
+        self.sessions[session.uuid] = session
+        return session.uuid
+
+    def train_model(self) -> None:
+        train = datasets.fetch_20newsgroups(
+            subset='train',
+            remove=('headers', 'footers', 'quotes'),
+            categories=CATEGORIES,
+            shuffle=False
+        )
+        
+        test = datasets.fetch_20newsgroups(
+            subset='test',
+            remove=('headers', 'footers', 'quotes'),
+            categories=CATEGORIES,
+            shuffle=False
+        )
+
+        print('Fitting TF-IDF vectorizer...')
+        vectorizer = TfidfVectorizer(norm=None, stop_words='english', ngram_range=(1,3), max_features=NUM_FEATURES)
+        vectorizer.fit(train.data)
+        print('Vectorizer fitting done. Vocabulary size: ' + str(len(vectorizer.vocabulary_)))
+
+        train_vectors = vectorizer.transform(train.data)
+        test_vectors = vectorizer.transform(test.data)
+
+        print('Fitting support vector classifier model...')
+        classifier = LinearSVC(C=0.00025, class_weight='balanced')
+        classifier.fit(train_vectors, train.target)
+        prediction = classifier.predict(train_vectors)
+        print('Model training done. Training accuracy: ' + str(metrics.f1_score(train.target, prediction, average='weighted')))
+        prediction = classifier.predict(test_vectors)
+        print('Test accuracy: ' + str(metrics.f1_score(test.target, prediction, average='weighted')))
+
+        self.vectorizer = vectorizer
+        self.classifier = classifier
+
+    def create_eva_program(self):
+        if self.vectorizer is None or self.classifier is None: 
+            print('No unencrypted model registered for encryption, no action taken...')
+            return
+
+        encrypted_svc = EvaProgram('resolve_svc', vec_size=NUM_FEATURES)
+        
+        with encrypted_svc:
+            x = Input('x')
+            x = x * self.vectorizer.idf_.tolist()
+
+            for category in range(len(CATEGORIES)):
+                x_weighted = x * self.classifier.coef_.tolist()[category]
+                x_summed = self.horizontal_sum(x_weighted)
+                x_intercept = x_summed + self.classifier.intercept_.tolist()[category]
+                Output('x{}'.format(category), x_intercept)
+
+        encrypted_svc.set_input_scales(40)
+        encrypted_svc.set_output_ranges(30)
+
+        self.program = encrypted_svc
+
+    @staticmethod
+    def horizontal_sum(x):
+        i = 1
+        while i < x.program.vec_size:
+            y = x << i
+            x = x + y
+            i <<= 1
+        return x
+        
+# Eva Session Object
+
+class EvaSession:
+    def __init__(self, parent: EvaApp) -> None:
+        self.state = 0
+        self.parent = parent
+        self.uuid = uuid.uuid4().hex
+        self.public_ctx_path = f'session.{self.uuid}.sealpublic'
+        self.public_ctx = None
+
+        compiler = CKKSCompiler()
+        compiled_prog, params, signature = compiler.compile(parent.program)
+        save(params, f'session.{self.uuid}.evaparams')
+        save(signature, f'session.{self.uuid}.evasignature')
+
+        self.compiled = compiled_prog
+        self.session_files = [self.public_ctx_path, f'session.{self.uuid}.evaparams', f'session.{self.uuid}.evasignature']
+
+    def load_public_ctx(self):
+        self.public_ctx = load(self.public_ctx_path)
+        self.state = 1
+
+    def execute(self, input_path):
+        self.session_files.append(input_path)
+        print('Loading input')
+        input = load(input_path)
+        print('Calculating output')
+        output = self.public_ctx.execute(self.compiled, input)
+        print('Saving output')
+        save(output, f'{input_path}.output')
+        self.session_files.append(f'{input_path}.output')
+    
+    def close(self):
+        for file in self.session_files:
+            if os.path.exists(file):
+                os.remove(file)
+
+        self.state = 2
+
+api = Flask(__name__)
+eva = EvaApp()
+
+# Global endpoints to retrieve vocabulary, signature and parameters
+
+@api.route('/vocabulary', methods=['GET'])
+def get_vocabulary():
+    if eva.vectorizer == None:
+        return {'error': 'Application is not ready to accept sessions yet. Please wait...'}, 418
+    else:
+        return str(eva.vectorizer.vocabulary_).replace('\'', '\''), 200
+
+@api.route('/categories', methods=['GET'])
+def get_categories():
+    return {'categories': list(CATEGORIES)}, 200
+
+
+# Session endpoints to handle session management for clients
+
+@api.route('/session', methods=['POST'])
+def create_session():
+    id = eva.new_session()
+
+    if id == '':
+        return {'error': 'Application is not ready to accept sessions yet. Please wait...', 'session_uuid':''}, 418
+    else:
+        return {'session_uuid': id}, 200
+
+@api.route('/session/<id>/public', methods=['POST'])
+def set_session_context(id):
+    if id not in eva.sessions:
+        return {'error': 'The requested session does not exist.', 'session_state':'-1'}, 404
+
+    if 'file' not in request.files:
+        return {'error': 'No context file provided.'}, 406
+
+    session = eva.sessions[id]
+    file = request.files['file']
+    file.save(session.public_ctx_path)
+    session.load_public_ctx()
+    return {'session_state': '1'}, 200
+
+@api.route('/session/<id>/evaluate', methods=['POST'])
+def evaluate_session_input(id):
+    if id not in eva.sessions:
+        return {'error': 'The requested session does not exist.', 'session_state':'-1'}, 404
+
+    if 'file' not in request.files:
+        return {'error': 'No input file provided.'}, 406
+
+    input_uuid = uuid.uuid4().hex
+    input_fname = f'session.{id}.{input_uuid}'
+    output_fname = f'session.{id}.{input_uuid}.output'
+
+    print(f'Evaluating input for session {id}.')
+    session = eva.sessions[id]
+    file = request.files['file']
+    file.save(input_fname)
+    session.execute(input_fname)
+
+    return_data = io.BytesIO()
+    with open(output_fname, 'rb') as fo:
+        return_data.write(fo.read())
+    return_data.seek(0)
+
+    os.remove(input_fname)
+    os.remove(output_fname)
+
+    return send_file(return_data, mimetype='application/octet-stream')
+
+@api.route('/session/<id>/state', methods=['GET'])
+def get_session_state(id):
+    if id not in eva.sessions:
+        return {'error': 'The requested session does not exist.', 'session_state':'-1'}, 404
+    else:
+        return {'session_state': eva.sessions[id].state}, 200
+
+@api.route('/session/<id>/close', methods=['POST'])
+def close_session(id):
+    if id not in eva.sessions:
+        return {'error': 'The requested session does not exist.', 'session_state':'-1'}, 404
+    else:
+        eva.sessions[id].close()
+        return {'session_state': eva.sessions[id].state}, 200
+
+@api.route('/session/<id>/signature', methods=['GET'])
+def get_signature(id):
+    return send_file(f'session.{id}.evasignature', as_attachment=True)
+
+@api.route('/session/<id>/params', methods=['GET'])
+def get_params(id):
+    return send_file(f'session.{id}.evaparams', as_attachment=True)
+
+if __name__ == '__main__':
+    api.run(threaded=False, host='0.0.0.0', port=80)
